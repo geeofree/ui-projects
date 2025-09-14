81369892b79483296aa1b633076de3e10d55f932
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2025-09-14 08:39:25 +0800

    chore: use eleventy v1 instead of latest v3 to fix cascade issues

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
+const path = require("path");
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default async function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
 	eleventyConfig.addPassthroughCopy({ 'src/projects/**/*.js': 'js' })
 
